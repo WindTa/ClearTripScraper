ada903475505fc88193fbefdb1a8281d155bbfa8
John Ta <john.ta@student.csulb.edu>
2019-12-10 17:19:50 -0800

    Changed HTML parser to lxml to improve performance

--- Scraper.py
+++ Scraper.py
@@ -12,7 +12,7 @@ def url_to_children(url):
     uClient = uReq(url)
     page_html = uClient.read()
     uClient.close()
-    page_soup = soup(page_html, "html.parser")
+    page_soup = soup(page_html, "lxml")
     page_children = page_soup.find("table", {"class": "results"}).findChildren("tr", recursive=True)
     return page_children
 
@@ -33,6 +33,8 @@ if __name__ == '__main__':
     df = pd.DataFrame(columns = ['No.', 'Station Code', 'Station Name', 'Arrival', 'Departure', 'Distance', 'Train No.'])
 
     # First page: https://www.cleartrip.com/trains/list?field=number&sort=up
+    
+    # Takes the URL and breaks down to the children we expect
     train_page_URL = input("Enter the URL for a page of trains: ")
     train_page_children = url_to_children(train_page_URL)
 
@@ -46,20 +48,23 @@ if __name__ == '__main__':
             station_page_URL = "https://www.cleartrip.com/trains/" + train_id
             # Accounts for url that redirects to an unexpected page
             if station_page_URL != requests.get(station_page_URL).url:
+                print("\tPage redirected. Going to next train.")
                 continue
+            print("\tTrain found. Going through stations.")
+            # Takes the URL and breaks down to the children we expect
             station_page_children = url_to_children(station_page_URL)
 
             # Go through every station a train goes through and saves all the data to a dictionary
             for station_child in station_page_children:
-                station_children = station_child.findChildren("td", recursive=False)
+                station_values = station_child.findChildren("td", recursive=False)
                 # Accounts for empty elements in the document
-                if len(station_children) != 0:
+                if len(station_values) != 0:
                     # Creates all values to add to dictionary
-                    station_no = str.strip(station_children[0].get_text())
-                    station_name, station_code = [" ".join(x.split()) for x in re.split(r'[()]', station_children[1].get_text()) if x.strip()]
-                    station_arrive = endpoint_check(station_children[2].get_text(), station_children[6].get_text())
-                    station_depart = endpoint_check(station_children[3].get_text(), station_children[6].get_text())
-                    station_distance = station_children[5].get_text()
+                    station_no = str.strip(station_values[0].get_text())
+                    station_name, station_code = [" ".join(x.split()) for x in re.split(r'[()]', station_values[1].get_text()) if x.strip()]
+                    station_arrive = endpoint_check(station_values[2].get_text(), station_values[6].get_text())
+                    station_depart = endpoint_check(station_values[3].get_text(), station_values[6].get_text())
+                    station_distance = station_values[5].get_text()
                     # Adds dictionary to the dataframe
                     df = df.append({'No.': station_no, 'Station Code': station_code, 'Station Name': station_name, 'Arrival': station_arrive, 'Departure': station_depart, 'Distance': station_distance, 'Train No.': train_id}, ignore_index=True)
     
